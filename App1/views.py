0a3b295fdb06258149d1f7c6a45042ea55c78e13
daniel fonseca <ignacio.fonseca2004@gmail.com>
2023-09-25 15:13:29 -0300

    App1 funcionando

---
+++ App1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def vista1(request):
+    html = "<h1 style='text-align:center; color:blue;'> Aplicacion 1 </h1>"
+    return HttpResponse(html)
--- difp_proyecto1/urls.py
+++ difp_proyecto1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App1 import views as a1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vista1/', a1.vista1)
+    
 ]
